ee20e64632d0f358e3dab346b03444b9e826d2e6
kmumfor2 <120679405+kmumfor2@users.noreply.github.com>
2023-10-12 14:37:51 -0400

    changed App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,17 @@
-import Nav from 'react-bootstrap/Nav'
-import Container from 'react-bootstrap/Container'
+import './App.css';
+import TopBar from './components/TopBar'
+import Background from './components/Background'
+import StoreListings from './components/StoreListings'
+
+
+function App() {
+  return (
+    <div className="App">
+      <TopBar />
+      <Background />
+      <StoreListings />
+    </div>
+  );
+}
 
 export default App;
